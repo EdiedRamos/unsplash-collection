99100694dd48046811aa3357dea8c3fdac4c1b7a
EdiedRamos <edieisa22@gmail.com>
2024-08-24 18:28:23 -0500

    feat: implement header

---
+++ app/(components)/header.tsx
@@ -0,0 +1,15 @@
+import { Logo } from "./logo";
+import { Navigation } from "./navigation";
+
+export function Header() {
+  return (
+    <header className="border-b-2">
+      <div className="flex flex-wrap justify-between items-center px-8 py-4">
+        <div>
+          <Logo />
+        </div>
+        <Navigation />
+      </div>
+    </header>
+  );
+}
---
+++ app/(components)/navigation.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const OPTIONS = [
+  {
+    id: "54daa298-e855-4203-aa07-50fad997fcac",
+    value: "Home",
+    to: "/",
+  },
+  {
+    id: "6be2be8b-1fa8-41d1-bf2c-45f69176894c",
+    value: "Collections",
+    to: "/collections",
+  },
+];
+
+export const Navigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex gap-2">
+      {OPTIONS.map((link) => (
+        <Link
+          key={link.id}
+          href={link.to}
+          className={`px-5 py-2 ${
+            pathname === link.to ? "bg-gray-200 rounded-md" : ""
+          }`}
+        >
+          {link.value}
+        </Link>
+      ))}
+    </nav>
+  );
+};
