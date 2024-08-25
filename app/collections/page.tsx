09df8bb58980f320b73c503283b5ab78b7a50f28
EdiedRamos <edieisa22@gmail.com>
2024-08-25 14:23:41 -0500

    feat: implement collections basic page

---
+++ app/collections/page.tsx
@@ -0,0 +1,17 @@
+export default function CollectionPage() {
+  return (
+    <div>
+      <h2 className="text-center text-cs-300 bg-gradient-image mt-9">
+        Collections
+      </h2>
+      <p className="text-black dark:text-white mx-auto text-center max-w-[400px] mt-2">
+        Explore the world through collections of beautiful photos free to use
+        under the{" "}
+        <a href="#" className="underline">
+          Unsplash License
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
